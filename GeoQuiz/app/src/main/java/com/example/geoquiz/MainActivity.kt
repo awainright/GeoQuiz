5976eb840e3d195d80d2ff74be13c57917b02e13
Adam Wainright <wainrighta16@students.ecu.edu>
2020-05-26 20:46:41 -0400

    CH.7

--- GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.geoquiz
 
 import android.app.Activity
+import android.app.ActivityOptions
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
@@ -52,10 +54,24 @@ class MainActivity : AppCompatActivity() {
             quizViewModel.moveToNext()
             updateQuestion()
         }
-        cheatButton.setOnClickListener {
+        cheatButton.setOnClickListener {view->
             val answerIsTrue = quizViewModel.currentQuestionAnswer
             val intent = CheatActivity.newIntent(this@MainActivity,answerIsTrue)
-            startActivityForResult(intent, REQUEST_CODE_CHEAT)
+
+
+            /**
+             * Checking build version constraint, happens at runtime
+             */
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                val options = ActivityOptions
+                    .makeClipRevealAnimation(view,0,0,view.width, view.height)
+                startActivityForResult(intent, REQUEST_CODE_CHEAT,options.toBundle())
+            }else{
+                startActivityForResult(intent, REQUEST_CODE_CHEAT)
+            }
+
+
         }
 
         updateQuestion()
